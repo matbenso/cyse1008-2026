1c035419904c6e327bb12f36b6591cea9e3d4d77
matbenso <bensonmathew@loyalistcollege.com>
2026-04-01 13:49:35 -0500

    Assignment 5: API routes and callbacks

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,13 +1,14 @@
 import { HomeView } from 'src/sections/home/view';
-
+ 
 // ----------------------------------------------------------------------
-
+ 
 export const metadata = {
   title: 'Loyalist: The starting point for your next project',
   description:
     'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
 };
-
+ 
 export default function Page() {
   return <HomeView />;
 }
+ 
